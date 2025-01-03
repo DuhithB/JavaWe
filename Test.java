0124e6987eeb472a05c5e22420b22665e4747522
DuhithB <duhithaboddeti@gmail.com>
2025-01-03 11:46:44 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,11 +2,9 @@ class Test
 {
  public static void main(String[] args)
  {
-  a=5;
-  b=6;
+
   System.out.println("Hello,Jenkins");
-  System.out.println("a:5,b:6");
-  System.out.println("Addition of nos:",a+b);
+
  }
 
 }
